e86ae1f34834397a8722c9f34a6b9d92d3537658
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-09-02 12:08:27 -0300

    sintaxe e tipos de dados

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,8 +1,61 @@
-﻿using ExemploFundamentos.Models;
+﻿using System;
+using ExemploFundamentos.Models;
 
-Pessoa pessoa1 = new Pessoa();
+//string a = "15=";
+//int b = 0;
+//int.TryParse(a, out b);  // Se a variavel a ser convertido com sucesso, imprimi normalmente se falar, a saida será a variavel b.
+//Console.WriteLine(b);
+//Console.WriteLine("Conversão realizada com sucesso!");
 
-pessoa1.Nome = "Gilberto";
-pessoa1.Idade = 32;
-pessoa1.Apresentar();
+//double a = 4/(2+2);
+//Console.WriteLine(a);
+
+//int a = 5;
+//double b = a;
+//long a = long.MaxValue;
+//int b = Convert.ToInt32(a);
+//int a = int.MaxValue;
+//long b = a;
+//Console.WriteLine(b);
+
+//int inteiro = 5;
+//string a = inteiro.ToString();
+//Console.WriteLine(a);
+
+//Cast - Casting
+//int a = Convert.ToInt32("5");
+//int a = int.Parse("5");
+//Console.WriteLine(a);
+
+//int a = 10;
+//int b = 20;
+//int c = a + b;
+//c = c + 5;
+//c += 5;
+//Console.WriteLine(c);
+
+
+//DateTime dataAtual = DateTime.Now.AddDays(5);
+//Console.WriteLine("A data de agora é " + dataAtual.ToString("dd/MM/yy  HH:mm"));
+
+
+//string apresentacao = "Olá mundo";
+//int qtd = 1;
+//Console.WriteLine("O valor da variavel quantidade :" + qtd);
+//qtd = 10;
+//Console.WriteLine("O valor da variavel quantidade :" + qtd);
+//double altura = 1.80;
+//decimal preco = 1.80M;
+//bool condicao = true;
+//Console.WriteLine(apresentacao);
+//Console.WriteLine("Valor da variavel quantidade " + qtd);
+//Console.WriteLine("Valor da variavel altura " + altura.ToString("0.00"));
+//Console.WriteLine("Valor da variavel preço " + preco);
+//Console.WriteLine("Valor da variavel condição " + condicao);
+
+
+//Pessoa pessoa1 = new Pessoa();
+//pessoa1.Nome = "Gilberto";
+//pessoa1.Idade = 32;
+//pessoa1.Apresentar();
 
